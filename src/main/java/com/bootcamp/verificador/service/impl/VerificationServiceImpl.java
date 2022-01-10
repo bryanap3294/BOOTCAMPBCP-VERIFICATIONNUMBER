7865e1dba3323cc8563b680dc00150d47f5045ea
Bryan <bryanap3294@gmail.com>
2022-01-10 13:53:38 -0500

    avance

--- src/main/java/com/bootcamp/verificador/service/impl/VerificationServiceImpl.java
+++ src/main/java/com/bootcamp/verificador/service/impl/VerificationServiceImpl.java
@@ -1,16 +1,24 @@
 package com.bootcamp.verificador.service.impl;
 
 import com.bootcamp.verificador.service.VerificationService;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
 public class VerificationServiceImpl implements VerificationService {
 
-
+    Integer[] sourceArray = {3, 2, 7, 6, 5, 4, 3, 2};
+    List<Integer> values = Arrays.asList(sourceArray);
 
     @Override
     public Integer getVerificationNumber(String dni) {
 
         String[] dniArray = dni.split("");
-
+        List<String> sourceList = Arrays.asList(dniArray);
 
         return null;
     }
